d3cd64868ecce35788db62bc9eb48c51b42ebbba
asia-jankowska00 <asia.jankowska00@gmail.com>
2020-11-17 08:47:48 +0100

    Add backend dependencies

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '39b2610d442c0e5e5bd1e692416932ef'),
+    },
+  },
+});
